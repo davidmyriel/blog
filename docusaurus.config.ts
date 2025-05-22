95bc344a51c2e2fdba5f69a3cc5a1eaff7de717e
David Myriel <davidmyriel@gmail.com>
2025-05-22 21:04:16 +0200

    Revert "add blog only"
    This reverts commit a59cd186006b7e421366608eab8f1a8b1eff99d7.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -35,9 +35,14 @@ const config: Config = {
     [
       'classic',
       {
-        docs: false, // Optional: disable the docs plugin
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
         blog: {
-          routeBasePath: '/',
           showReadingTime: true,
           feedOptions: {
             type: ['rss', 'atom'],
@@ -63,12 +68,25 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'LanceDB Blog',
+      title: 'My Site',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
       },
-      items: [],
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Tutorial',
+        },
+        {to: '/blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
     },
     footer: {
       style: 'dark',
